3a6b37d8632847d2922e99fe4e9d95fcadf6dec3
Marlo Deocampo <marlo.deocampo@gmail.com>
2026-07-05 17:25:18 +0900

    Fix Flask binding to 0.0.0.0 and PORT env var for Render

--- server.py
+++ server.py
@@ -1292,4 +1292,5 @@ def static_files(filename):
 _ensure_initial_user()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
